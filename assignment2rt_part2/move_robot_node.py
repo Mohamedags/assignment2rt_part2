d88d62a862d6966de349522f4afeb2e223072526
Mohamedags <aguenarousm26@gmail.com>
2024-12-30 21:31:08 +0100

    Making the robot moving only for 5s then asks the user again if you want to give another values

--- assignment2rt_part2/move_robot_node.py
+++ assignment2rt_part2/move_robot_node.py
@@ -1,6 +1,7 @@
 import rclpy
 from rclpy.node import Node
 from geometry_msgs.msg import Twist
+import time
 
 class MoveRobotNode(Node):
     def __init__(self):
@@ -9,7 +10,7 @@ class MoveRobotNode(Node):
         self.get_logger().info("MoveRobotNode has been started")
         
         self.velocity = Twist()
-        
+
     def set_velocity(self, linear_x, linear_y, linear_z, angular_roll, angular_pitch, angular_yaw):
         """Set the linear and angular velocity for the robot."""
         self.velocity.linear.x = linear_x
@@ -20,28 +21,51 @@ class MoveRobotNode(Node):
         self.velocity.angular.z = angular_yaw
         self.get_logger().info(f"Setting velocity: linear=({linear_x}, {linear_y}, {linear_z}), angular=({angular_roll}, {angular_pitch}, {angular_yaw})")
 
-    def send_velocity(self):
-        """Send the velocity command."""
+    def send_velocity_for_5s(self):
+        """Send velocity command for 5 seconds."""
+        start_time = self.get_clock().now().seconds_nanoseconds()[0]
+        
+        while self.get_clock().now().seconds_nanoseconds()[0] - start_time < 5:
+            self.publisher_.publish(self.velocity)
+            time.sleep(0.1)  
+            
+        self.get_logger().info("Stopping the robot after 5 seconds.")
+        self.stop_robot()
+
+    def stop_robot(self):
+        """Stop the robot by setting velocity to zero."""
+        self.velocity.linear.x = 0.0
+        self.velocity.linear.y = 0.0
+        self.velocity.linear.z = 0.0
+        self.velocity.angular.x = 0.0
+        self.velocity.angular.y = 0.0
+        self.velocity.angular.z = 0.0
         self.publisher_.publish(self.velocity)
-        self.get_logger().info("Publishing velocity command.")
+        self.get_logger().info("Robot stopped.")
 
 def main(args=None):
     rclpy.init(args=args)
     node = MoveRobotNode()
     
     try:
-        # User interface : input for linear and angular velocities (6 values)
-        linear_x = float(input("Enter linear velocity X (m/s): "))
-        linear_y = float(input("Enter linear velocity Y (m/s): "))
-        linear_z = float(input("Enter linear velocity Z (m/s): "))
-        
-        angular_roll = float(input("Enter angular velocity Roll (rad/s): "))
-        angular_pitch = float(input("Enter angular velocity Pitch (rad/s): "))
-        angular_yaw = float(input("Enter angular velocity Yaw (rad/s): "))
-        
-        node.set_velocity(linear_x, linear_y, linear_z, angular_roll, angular_pitch, angular_yaw)
-        
-        node.send_velocity()
+        while True:  
+            # User interface : input for linear and angular velocities (6 values)
+            print("\nEnter new velocities for the robot:")
+            linear_x = float(input("Enter linear velocity X (m/s): "))
+            linear_y = float(input("Enter linear velocity Y (m/s): "))
+            linear_z = float(input("Enter linear velocity Z (m/s): "))
+            
+            angular_roll = float(input("Enter angular velocity Roll (rad/s): "))
+            angular_pitch = float(input("Enter angular velocity Pitch (rad/s): "))
+            angular_yaw = float(input("Enter angular velocity Yaw (rad/s): "))
+            
+            node.set_velocity(linear_x, linear_y, linear_z, angular_roll, angular_pitch, angular_yaw)
+                     
+            node.send_velocity_for_5s()
+            
+            user_input = input("\nDo you want to enter new velocities? (yes/no): ").strip().lower()
+            if user_input != "yes":
+                break
         
     except KeyboardInterrupt:
         pass
